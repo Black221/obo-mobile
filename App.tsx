c8162fb22c485335896b0d9cde1a0661514c34bf
Black221 <bambabar02@gmail.com>
2023-11-10 17:58:28 +0000

    loading screen manage

--- App.tsx
+++ App.tsx
@@ -4,6 +4,8 @@ import { StatusBar } from 'expo-status-bar';
 import config from './tamagui.config'
 import StackNav from '@/navigation/StackNav';
 import { NavigationContainer } from '@react-navigation/native';
+import LoadingScreen from '@/screens/loading';
+import { useEffect, useState } from 'react';
 
 /* CAS CONTEXT */
 export type ReactChildren = React.ReactNode
@@ -19,17 +21,29 @@ export default function App() {
 		InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
 	})
 
-	if (!loaded) {
-		return null
+  //wait 10s before rendering
+  const [render, setRender] = useState(false)
+
+  useEffect(() => {
+    const id = setTimeout(() => {
+      setRender(true)
+      clearTimeout(id)
+    }, 10000)
+  }, [])
+
+	if (!loaded || !render) {
+		return <LoadingScreen />
 	}
 
   return (
     <TamaguiProvider config={config}>
       <Theme name="light">
         {/* rendu */}
+
         <NavigationContainer>
           <StackNav />
         </NavigationContainer>
+        
 			</Theme>
 			<StatusBar style="light" backgroundColor='black' animated={true}  hidden={false} translucent={false} />
     </TamaguiProvider>
